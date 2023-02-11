1f7c51f3d207eaa88fffcbfae2a1307c854a9043
StanimalTheMan <sjc591@nyu.edu>
2023-02-10 21:34:15 -0500

    try to seed db and add route/route groups

---
+++ prisma/seed.ts
@@ -0,0 +1,57 @@
+import { db } from "@/lib/db";
+import { TODO_STATUS } from "@prisma/client";
+
+const getRandomTodoStatus = () => {
+  const statuses = [
+    TODO_STATUS.COMPLETED,
+    TODO_STATUS.NOT_COMPLETED
+  ];
+  return statuses[Math.floor(Math.random() * statuses.length)];
+};
+
+async function main() {
+  const user = await db.user.upsert({
+    where: { email: "user@email.com" },
+    update: {},
+    create: {
+      email: "user@email.com",
+      firstName: "User",
+      lastName: "Person",
+      password: "password",
+      places: {
+        create: new Array(5).fill(1).map((_, i) => ({
+          name: `Place ${i}`,
+        })),
+      },
+    },
+    include: {
+      places: true,
+    },
+  });
+
+  const todos = await Promise.all(
+    user.places.map((place) =>
+      db.todo.createMany({
+        data: new Array(10).fill(1).map((_, i) => {
+          return {
+            name: `Todo ${i}`,
+            ownerId: user.id,
+            placeId: place.id,
+            status: getRandomTodoStatus(),
+          };
+        }),
+      })
+    )
+  );
+
+  console.log({ user, todos });
+}
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
