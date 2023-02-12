15d5075c32d44782bd787e1e4dd49b5d41f0132d
StanimalTheMan <sjc591@nyu.edu>
2023-02-12 01:23:22 -0500

    update static text

--- components/NewPlace.tsx
+++ components/NewPlace.tsx
@@ -22,7 +22,7 @@ const NewPlace = () => {
 
   return (
     <div className="px-6 py-8 hover:scale-105 transition-all ease-in-out duration-200 flex justify-center items-center">
-      <Button onClick={() => openModal()}>+ New Project</Button>
+      <Button onClick={() => openModal()}>+ New Place</Button>
 
       <Modal
         isOpen={isModalOpen}
@@ -30,10 +30,10 @@ const NewPlace = () => {
         overlayClassName="bg-[rgba(0,0,0,.4)] flex justify-center items-center absolute top-0 left-0 h-screen w-screen"
         className="w-3/4 bg-white rounded-xl p-8"
       >
-        <h1 className="text-3xl mb-6">New Project</h1>
+        <h1 className="text-3xl mb-6">New Place</h1>
         <form className="flex items-center" onSubmit={handleSubmit}>
           <Input
-            placeholder="project name"
+            placeholder="place name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
