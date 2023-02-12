7fcd651474917725479c96ce90b518c664606170
StanimalTheMan <sjc591@nyu.edu>
2023-02-12 01:36:42 -0500

    update README and comment out some seed logic

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) aimed to help manage your next travel destination trip better.
 
-Improvements:
+[Link](https://travel-planner-app.vercel.app/)
+first name: user
+last name: userlast
+email: user@email.com
+password: testpass123
+
+Possible Improvements:
 - Use Google api to display map to more easily facilitate adding new place to visit.
 - Use calendar icon in sidebar to plan when to visit place by ideally, add datetimes, maybe display a calendar UI as well
-- Add due on each todo to order todos at a place better
+- Add due on each todo to better order todos at a place
+- Log out
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,42 +11,42 @@ const getRandomTodoStatus = () => {
 };
 
 async function main() {
-  const user = await db.user.upsert({
-    where: { email: "user@email.com" },
-    update: {},
-    create: {
-      email: "user@email.com",
-      firstName: "User",
-      lastName: "Person",
-      password: await hashPassword("password"),
-      places: {
-        create: new Array(5).fill(1).map((_, i) => ({
-          name: `Place ${i}`,
-        })),
-      },
-    },
-    include: {
-      places: true,
-    },
-  });
+  // const user = await db.user.upsert({
+  //   where: { email: "user@email.com" },
+  //   update: {},
+  //   create: {
+  //     email: "user@email.com",
+  //     firstName: "User",
+  //     lastName: "Person",
+  //     password: await hashPassword("password"),
+  //     places: {
+  //       create: new Array(5).fill(1).map((_, i) => ({
+  //         name: `Place ${i}`,
+  //       })),
+  //     },
+  //   },
+  //   include: {
+  //     places: true,
+  //   },
+  // });
 
-  const todos = await Promise.all(
-    user.places.map((place) =>
-      db.todo.createMany({
-        data: new Array(10).fill(1).map((_, i) => {
-          return {
-            name: `Todo ${i}`,
-            ownerId: user.id,
-            placeId: place.id,
-            description: `Everything that describes Todo ${i}`,
-            status: getRandomTodoStatus(),
-          };
-        }),
-      })
-    )
-  );
+  // const todos = await Promise.all(
+  //   user.places.map((place) =>
+  //     db.todo.createMany({
+  //       data: new Array(10).fill(1).map((_, i) => {
+  //         return {
+  //           name: `Todo ${i}`,
+  //           ownerId: user.id,
+  //           placeId: place.id,
+  //           description: `Everything that describes Todo ${i}`,
+  //           status: getRandomTodoStatus(),
+  //         };
+  //       }),
+  //     })
+  //   )
+  // );
 
-  console.log({ user, todos });
+  // console.log({ user, todos });
 }
 main()
   .then(async () => {
